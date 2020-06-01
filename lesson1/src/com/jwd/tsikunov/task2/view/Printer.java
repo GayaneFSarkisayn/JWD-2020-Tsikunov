b27dcea20df2f2040c5543731c7d792168f5c94c
NikolayTsikunov <nikolay.tsikunov@mail.ru>
2020-06-01 15:50:19 +0300

    update second task

--- lesson1/src/com/jwd/tsikunov/task2/view/Printer.java
+++ lesson1/src/com/jwd/tsikunov/task2/view/Printer.java
@@ -2,6 +2,10 @@ package com.jwd.tsikunov.task2.view;
 
 public class Printer {
     public static void print(int month, int year, int days) {
-        System.out.printf("In %d month %d year - %d days\n", month, year, days);
+        if(days == -1) {
+            System.out.printf("Incorrect data\n");
+        } else {
+            System.out.printf("In %d month %d year - %d days\n", month, year, days);
+        }
     }
 }
